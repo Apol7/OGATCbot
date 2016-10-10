b20b5873b27da70e06cb85a201affc886054fd8e
gtarpenning <gtarpenning@gmail.com>
2016-10-09 18:16:26 -0700

    Testing
    The testing Supreme code

---
+++ text.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+
+productKeyword = 'Reversible Hooded'
+supreme = 'http://www.supremenewyork.com/shop/all'
+category = '/jackets'
+desiredColor = "Black"
+
+print(datetime.datetime.now())
+driver = webdriver.Chrome()
+driver.get(supreme + category)
+
+specificproduct = driver.find_element_by_partial_link_text(productKeyword)
+print(supreme + category + specificproduct.get_attribute("href"))
+driver.get(specificproduct.get_attribute("href"))
+
+
+specificProductColor = driver.find_element_by_css_selector()
+print(supreme + category + specificProductColor.get_attribute("href"))
+driver.get(specificProductColor.get_attribute("href"))
+
+
+#elem.send_keys("level 12, right here")
+#elem.send_keys(Keys.RETURN)
+#time.sleep(2)
+
+print(datetime.datetime.now())
+
+
